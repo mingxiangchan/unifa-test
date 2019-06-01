2a81d8a6d63cd0fb681561febb1d98601c4fbd3f
Chan Ming Xiang <mingxiangchan@gmail.com>
2019-06-01 17:23:11 +0800

    clear user session on logout, redirect to login page
    redirect to login page instead of home to prevent flash error message

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "/sessions/new", to: "sessions#new"
   post "/sessions/new", to: "sessions#create"
+  delete "/sessions", to: "sessions#destroy"
   get "/pictures/new", to: "pictures#new"
   post "/pictures/new", to: "pictures#create"
   root "pictures#index"
