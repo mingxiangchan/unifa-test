ea5f9e85db212b1ea8aa4e047a2c6ff1bd4e32e3
Chan Ming Xiang <mingxiangchan@gmail.com>
2019-06-01 16:19:13 +0800

    happy path for creating picture

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,8 +1,22 @@
 class PicturesController < ApplicationController
   def new
+    @picture = Picture.new
     @errors = []
   end
 
   def create
+    @picture = Picture.new(picture_params)
+    @picture.user = current_user
+
+    if @picture.save
+      redirect_to "/"
+    else
+      @errors = @picture.errors.full_messages
+      render :new
+    end
+  end
+
+  def picture_params
+    params.require(:picture).permit(:title, :image)
   end
 end
