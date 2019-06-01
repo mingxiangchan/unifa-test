fdfb5bdb8999ad90f970ac24aef0fdcc01f0f93f
Chan Ming Xiang <mingxiangchan@gmail.com>
2019-06-01 15:09:15 +0800

    happy path for user login process

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,25 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user, @errors = LoginUser.new(user_params).run
+
+    if @errors.empty?
+      session[:user_id] = @user.id
+      redirect_to "/"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:identifier, :password).to_h.symbolize_keys
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  resources :sessions, only: [:new, :create, :destroy]
 end
