d0f0882861d56a69285ea4ada359c7d4582cda6d
Chan Ming Xiang <mingxiangchan@gmail.com>
2019-06-01 16:23:33 +0800

    validate image presence on picture creation

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -22,4 +22,13 @@ class Picture < ApplicationRecord
   has_one_attached :image
 
   validates :title, presence: true, length: { maximum: 30 }
+  validate :image_presence
+
+  private
+
+  def image_presence
+    if !image.attached?
+      errors.add(:image, "must be provided")
+    end
+  end
 end
